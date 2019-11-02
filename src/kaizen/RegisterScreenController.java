c61f205b2f876b375628b283a56bb41fac0ae469
wongad1 <wongad1@AU2344700W1.ey.net>
2019-11-02 16:26:23 +1100

    updated registerscreencontroller. Initiated @FXMLs

--- src/kaizen/RegisterScreenController.java
+++ src/kaizen/RegisterScreenController.java
@@ -5,10 +5,48 @@
  */
 package kaizen;
 
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import kaizen.UserData.KaizenDatabase;
+
 /**
  *
  * @author Raymond
  */
-public class RegisterScreenController {
+public class RegisterScreenController{
     
+    @FXML
+    private TextField email;
+    
+    @FXML
+    private TextField username;
+    
+    @FXML
+    private PasswordField pw; 
+    
+    @FXML
+    private PasswordField confirmPw;
+    
+    @FXML
+    private Button cancelRegistration;
+    
+    @FXML
+    private Button signUp;
+    
+    KaizenDatabase userDB = new KaizenDatabase();
+    
+    PageSwitchHelper pageSwitcher = new PageSwitchHelper();
+    
+    }
+
+@FXML
+private void handleCancel(ActionEvent event) throws IOException{
+pageSwitchHelper.switcher(event, "LoginScreen.fxml");
+}
+
+public String getLoggedInUser(){
+return loggedInUser;
 }
