d62a5d0df4eae0e8c87a760394cba661b1be6ce4
wongad1 <wongad1@AU2344700W1.ey.net>
2019-11-03 22:53:07 +1100

    daily learnings fxml

---
+++ src/kaizen/DailyLearningsController.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kaizen;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+
+/**
+ * FXML Controller class
+ *
+ * @author wongad1
+ */
+public class DailyLearningsController implements Initializable {
+
+    @FXML
+    private ComboBox answerOne;
+    
+    
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
