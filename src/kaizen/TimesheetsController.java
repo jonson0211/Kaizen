c3df375ae5d0371cff526d47669b5ceb9592443e
lienzhu <lienzhu@192.168.1.3>
2019-11-04 16:57:55 +1100

    Started TimesheetsController code

--- src/kaizen/TimesheetsController.java
+++ src/kaizen/TimesheetsController.java
@@ -1,27 +1,78 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package kaizen;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import kaizen.UserData.KaizenDatabase;
 
-/**
- * FXML Controller class
- *
- * @author lienzhu
- */
 public class TimesheetsController implements Initializable {
 
+    @FXML
+    private RadioButton categoryWork;
+    
+    @FXML
+    private RadioButton categoryDaily;
+    
+    @FXML
+    private RadioButton categoryRelationships;
+    
+    @FXML
+    private RadioButton categoryWellness;
+    
+    @FXML
+    private RadioButton categoryRelaxation;
+    
+    @FXML
+    private RadioButton categoryProjects;
+    
+    @FXML
+    private TextField timeStart;
+    
+    @FXML
+    private TextField timeEnd;
+    
+    @FXML
+    private TextArea description;
+    
+    @FXML
+    private Button submit;
+    
+    KaizenDatabase addTimesheet = new KaizenDatabase();
+    
+    PageSwitchHelper pageSwitcher = new PageSwitchHelper();
+    
     /**
      * Initializes the controller class.
      */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+    @FXML
+    private void handleSubmitAction(ActionEvent event) {
+        String timeStart = start.getText().trim();
+        String timeEnd = end.getText();
+        String description = descr.getText();
+        /** INSERT instead of SELECT?**/
+        try {
+            ResultSet rs = addTimesheet.getResultSet("SELECT * FROM T WHERE "
+                    + "USERNAME = '" + timeStart + "' "
+                    + "AND PASSWORD = '" + timeEnd + "'");
+            if (!rs.next()) {
+                loginOutput.setText("Incorrect username or password");
+                loginOutput.setVisible(true);
+            } else {
+                loginOutput.setText("Login successful");
+                loginOutput.setVisible(true);
+                nextBtn.setVisible(true);
+            }
+            rs.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+    }
 }
