a1d8881e5ddf3b41e96477e3205c79a329973f48
jonso <jonso@DESKTOP-N98NQPP.modem>
2019-11-04 20:50:39 +1100

    Merge origin/master

--- src/kaizen/KanbanBoardController.java
+++ src/kaizen/KanbanBoardController.java
@@ -5,10 +5,59 @@
  */
 package kaizen;
 
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ToggleButton;
+
 /**
  *
  * @author Raymond
  */
 public class KanbanBoardController {
+    
+    
+    PageSwitchHelper psh = new PageSwitchHelper();
+    
+    @FXML
+    private Button signOutButton;
+    @FXML
+    private ToggleButton kanbanBoard;
+    @FXML
+    private ToggleButton deepFocus;
+    @FXML
+    private ToggleButton taskTracker;
+    @FXML
+    private ToggleButton about;
+    @FXML
+    private ToggleButton timeDashboard;
+    @FXML
+    private ToggleButton doDateView;
+    @FXML
+    private ToggleButton dueDateView;
+    @FXML
+    private Label welcome;
+    @FXML
+    private Label welcomeSubheading;
+    
+    
+    //method to change the scene from do date mode to due date mode
+    @FXML
+    public void handleDueDateView(ActionEvent event) throws IOException {
+        psh.switcher(event, "KanbanBoardDueDateView.fxml");
+    }
+    
+    //method to change the scene from due date mode back to the default do date mode
+    @FXML
+    public void handleDoDateView(ActionEvent event) throws IOException {
+        psh.switcher(event, "KanbanBoardDateView.fxml");
+    }
+    
+    @FXML
+    public void initialize() {
+        System.out.println("OAIdfjif");
+    }
     
 }
