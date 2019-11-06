3706a8a56941008eadb08868a2bda160a45b4c94
wongad1 <wongad1@AU2344700W1.ey.net>
2019-11-07 00:03:01 +1100

    created data model for learnings did well and learnings to be better. Added code to populate table of daily learnings

--- src/kaizen/DataModels/learningsDoBetter.java
+++ src/kaizen/DataModels/learningsDoBetter.java
@@ -9,6 +9,37 @@ package kaizen.DataModels;
  *
  * @author wongad1
  */
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+/**
+ *
+ * @author wongad1
+ */
 public class learningsDoBetter {
+    private StringProperty doBetter;
+    private IntegerProperty doBetterCount;
+
+    public learningsDoBetter(String doBetter, int count){
+        this.doBetter = new SimpleStringProperty(doBetter);
+        this.doBetterCount = new SimpleIntegerProperty(count);
+    } 
+
+    public StringProperty getDoBetter() {
+        return doBetter;
+    }
     
+    public String getDoBetterProperty(){
+        return doBetter.get();
+    }
+    
+    public IntegerProperty getDoBetterCount() {
+        return doBetterCount;
+    }
+    public Integer getDoBetterCountProperty(){
+        return doBetterCount.get();
+        
+    }  
 }
