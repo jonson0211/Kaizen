18596e947b07f03a979d4cbd38a196bba57adb9a
lienzhu <lienzhu@192.168.1.3>
2019-11-09 22:37:33 +1100

    Timesheets edits

--- src/kaizen/TimesheetsController.java
+++ src/kaizen/TimesheetsController.java
@@ -31,7 +31,8 @@ import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 
 public class TimesheetsController implements Initializable {
-    @FXML
+   
+    /**@FXML
     private ComboBox<String> timeStartHourComboBox;        
     ObservableList<String> TSH = FXCollections.observableArrayList("1", "2","3","4","5","6","7",
             "8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24");
@@ -48,6 +49,7 @@ public class TimesheetsController implements Initializable {
     private ComboBox<String> timeEndMinComboBox;
     ObservableList<String> TEM = FXCollections.observableArrayList("0","5","10","15","20","25","30",
             "35","40", "45","50","55");
+    **/
     
     @FXML private AnchorPane anchor;
     @FXML private BorderPane border;
@@ -76,6 +78,7 @@ public class TimesheetsController implements Initializable {
     private ToggleButton about;  
     @FXML
     private ToggleButton signOut;  
+    /**
     @FXML
     private RadioButton categoryWork;
     @FXML
@@ -92,17 +95,40 @@ public class TimesheetsController implements Initializable {
     
     @FXML
     private RadioButton categoryProjects;
-    
-    @FXML private TextField timeStart;
-    
-    @FXML private TextField timeEnd;
+     **/
+    @FXML
+    private ToggleButton categoryWork;
     
     @FXML
-    private Label duration;
+    private ToggleButton categoryDaily;
+    
+    @FXML
+    private ToggleButton categoryRelationships;
+    
+    @FXML
+    private ToggleButton categoryWellness;
+    
+    @FXML
+    private ToggleButton categoryRelaxation;
+    
+    @FXML
+    private ToggleButton categoryProjects;
+    
+    
+    
+    @FXML private TextField timeStartHrField;
+    
+    @FXML private TextField timeEndHrField;
+    
+    @FXML private TextField timeStartMinField;
+    @FXML private TextField timeEndMinField;
+    
+    @FXML
+    private Label durationLabel;
     
     
     @FXML
-    private TextArea description;
+    private TextArea descriptionText;
     
     @FXML
     private Button submit;
@@ -110,8 +136,7 @@ public class TimesheetsController implements Initializable {
     @FXML
     private Button back;
     
-    
-    
+     
     ToggleGroup toggleGroup = new ToggleGroup();
  
     KaizenDatabase addTimesheet = new KaizenDatabase();
@@ -125,57 +150,85 @@ public class TimesheetsController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        timeStartHourComboBox.setItems(TSH);
-        timeStartMinComboBox.setItems(TSM);
-        timeEndHourComboBox.setItems(TEH);
-        timeEndMinComboBox.setItems(TEM);
-    TextArea description = new TextArea();
+        //timeStartHourComboBox.setItems(TSH);
+        //timeStartMinComboBox.setItems(TSM);
+        //timeEndHourComboBox.setItems(TEH);
+        //timeEndMinComboBox.setItems(TEM);
+    /**TextArea description = new TextArea();
     description.getText();
     System.out.print(description);
+    * **/
+    //durationLabel.setVisible(false);
        //TextArea description = new TextArea();
          
     }
-   
+    
     
     @FXML
     private void handleSubmitAction(ActionEvent event) {
         /** String start = timeStart.getText();
         String end = timeEnd.getText(); **/
         
-        String startHR = timeStartHourComboBox.getValue();
-        String endHR = timeEndHourComboBox.getValue();
-        String startMin = timeStartMinComboBox.getValue(); 
-        String endMin = timeEndMinComboBox.getValue();
+        //String startHR = timeStartHourComboBox.getValue();
+        //String endHR = timeEndHourComboBox.getValue();
+        //String startMin = timeStartMinComboBox.getValue(); 
+        //String endMin = timeEndMinComboBox.getValue();
         
-        String combinedStart = startHR.concat(":"+ startMin);
-        String combinedEnd = endHR.concat(":"+ endMin);
+        //String combinedStart = startHR.concat(":"+ startMin);
+        //String combinedEnd = endHR.concat(":"+ endMin);
         
-        int startHRTime = Integer.parseInt(startHR);
-        int endHRTime = Integer.parseInt(endHR);
-        int startMinTime = Integer.parseInt(startMin);
-        int endMinTime = Integer.parseInt(endMin);
+        //int startHRTime = Integer.parseInt(startHR);
+        //int endHRTime = Integer.parseInt(endHR);
+        //int startMinTime = Integer.parseInt(startMin);
+        //int endMinTime = Integer.parseInt(endMin);
         
-        int startSum = (startHRTime*60 + startMinTime);
-        int endSum =(endHRTime*60 + endMinTime);
-        int duration = (endSum - startSum);
+        //int startSum = (startHRTime*60 + startMinTime);
+        //int endSum =(endHRTime*60 + endMinTime);
+        //int duration = (endSum - startSum);
         
+        Toggle catName = toggleGroup.getSelectedToggle();
+        String timeStartHr = timeStartHrField.getText();
+        String timeStartMin = timeStartMinField.getText();
+        String timeEndHr = timeEndHrField.getText();
+        String timeEndMin = timeEndMinField.getText();
+        
+        double timeStartHrNum = Double.parseDouble(timeStartHr);
+        double timeStartMinNum = Double.parseDouble(timeStartMin);
+        double timeEndHrNum = Double.parseDouble(timeEndHr);
+        double timeEndMinNum = Double.parseDouble(timeEndMin);
+        
+        double startCombined = (timeStartHrNum*60 + timeStartMinNum);
+        double endCombined = (timeEndHrNum*60 + timeEndMinNum);
+        
+        double duration = endCombined - startCombined; 
         
        
-        Toggle cat;
-        cat = toggleGroup.getSelectedToggle();
-        String desc;
+        String durationText = Double.toString(duration);
+        String desc = descriptionText.getText();
+        
+        //Toggle cat;
+        //cat = toggleGroup.getSelectedToggle();
+        
         //desc = description.getText();
         //System.out.println(desc);
 
         try {
-            addTimesheet.insertStatement("INSERT INTO TIMESHEETS (START, END, DURATION, DESCR, CATEGORYNAME)"
-                    + " VALUES(" + combinedStart + ", "+  combinedEnd + ", " +
-                    //duration+ 
-                    ", " 
-                    //+ desc + ", "
-                    + cat);
+            System.out.print(duration);
+            System.out.print(startCombined);
+            System.out.print(endCombined);
+            System.out.print(catName);
+            System.out.println(desc);
+            System.out.println(duration);
+            
+            addTimesheet.insertStatement("INSERT INTO TIMESHEETS (CATEGORYNAME, START, END, DURATION, DESCR)"
+                    + " VALUES('"+ catName + "', '"+ startCombined + "', '"+  endCombined + "', '" +
+                    duration + "', '" + desc + "');");
+            durationLabel.setText(durationText);
+            durationLabel.setVisible(true);
+            
+            
        //**test if duration is calculated**// 
-       System.out.println(duration);
+       
         } catch (Exception ex) {
             ex.printStackTrace();
         }
